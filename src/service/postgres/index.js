19d1a8b87d577767d35f3124f8b6b3d3ccd4362d
KristofaJosh <christopherjoshua18@gmail.com>
2020-07-08 15:06:27 +0100

    push herokut db config

--- src/service/postgres/index.js
+++ src/service/postgres/index.js
@@ -1,7 +1,16 @@
 const pgtools = require('pgtools');
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
-const config = {
+let herokuDB = {
+    user: process.env.P_POSTGRES_USER,
+    password: process.env.P_POSTGRES_PASSWORD,
+    database: process.env.P_POSTGRES_DB,
+    host: process.env.P_HOST,
+    port: '5432',
+};
+
+const local = {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
@@ -9,20 +18,33 @@ const config = {
     port: '',
 };
 
+let db = 'starwars'
+let config = `postgres://${process.env.P_POSTGRES_USER}:${process.env.P_POSTGRES_PASSWORD}@${process.env.P_HOST}:5432/${process.env.P_POSTGRES_DB};`
+if(process.env.NODE_ENV === 'development'){
+    config = local
+}else {
+    config = herokuDB
+    db =  process.env.POSTGRES_DB;
+}
+
 const pool = new Pool(config);
 
-pgtools.createdb(config, 'starwars', function (err, res) {
-    if (err) {return} // db created already
+pgtools.createdb(config, db, function (err, res) {
+    if (err) {
+        return
+    } // db created already
     console.log(res);
 });
 
+
 const query = `
-    CREATE TABLE IF NOT EXISTS comments (
-        id serial,
-        ip varchar,
-        episode varchar,
-        comment varchar,
-        created_at varchar                             
+    CREATE TABLE IF NOT EXISTS comments
+    (
+        id         serial,
+        ip         varchar,
+        episode    varchar,
+        comment    varchar,
+        created_at varchar
     );
 `;
 
